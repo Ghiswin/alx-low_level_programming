44eea69270acc489bb69ab3c4630f7171217a05c
Ghiswin <adonnguessan@gmail.com>
2022-07-06 00:32:37 +0000

    putchar 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - print the alphabet in lowercase
+*
+* return: 0
+*/
+
+void print_alphabet(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
